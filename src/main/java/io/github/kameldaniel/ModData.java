2212e57c8664c07b998fe32c38152349e72e8536
KamelDaniel <daniel.kamel@rogers.com>
2025-10-04 10:15:12 -0400

    About to try getting unreachable ids from items with server events

--- src/main/java/io/github/kameldaniel/ModData.java
+++ src/main/java/io/github/kameldaniel/ModData.java
@@ -1,7 +1,62 @@
 package io.github.kameldaniel;
 
+import com.mojang.serialization.Codec;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.PersistentState;
+import net.minecraft.world.PersistentStateType;
+import net.minecraft.world.World;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Stack;
 
 public class ModData extends PersistentState {
-    
+    public int nextDimID = 0;
+    Stack<Integer> unreachableIDs = new Stack<>();
+
+    private ModData() {}
+
+    private ModData(String data) {
+        Arrays.stream(data.split("\\s+")).mapToInt(Integer::valueOf).forEach(unreachableIDs::push);
+        this.nextDimID = this.unreachableIDs.pop();
+    }
+
+    private String getSaveString() {
+        StringBuilder saveString = new StringBuilder();
+        while (!this.unreachableIDs.empty()) {
+            saveString.append(this.unreachableIDs.pop()).append(" ");
+        }
+        saveString.append(this.nextDimID);
+        return saveString.toString();
+    }
+
+    private static final Codec<ModData> CODEC = Codec.STRING.fieldOf("dim_ids").codec().xmap(
+            ModData::new,
+            ModData::getSaveString
+    );
+
+    private static final PersistentStateType<ModData> type = new PersistentStateType<>(
+            PocketContraptions.MOD_ID,
+            ModData::new,
+            CODEC,
+            null
+    );
+
+    public static ModData getModData(MinecraftServer server) {
+        ServerWorld world = server.getWorld(World.OVERWORLD);
+        Objects.requireNonNull(world);
+        ModData modData = world.getPersistentStateManager().getOrCreate(ModData.type);
+        modData.markDirty();
+        return modData;
+    }
+
+    public int getDimID() {
+        if (this.unreachableIDs.empty()) return nextDimID++;
+        else return this.unreachableIDs.pop();
+    }
+
+    public void addUnreachable(int unreachableID) {
+        this.unreachableIDs.push(unreachableID);
+    }
 }
